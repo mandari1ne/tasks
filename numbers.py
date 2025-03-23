1ebd4efe36321883fb6d9b248f77968448f9e187
mandari1ne <marina.stakhovets@gmail.com>
2025-03-23 18:05:12 +0300

    optimise cost of rocks

--- numbers.py
+++ numbers.py
@@ -1,9 +1,5 @@
 def count_nums(n):
-    count_ = 0
-    for i in range(1, n + 1):
-        count_ += len(str(i))
-
-    return count_
+    return sum(len(str(i)) for i in range(1, n + 1))
 
 
 n = int(input())
